139526d7d4d20256940101a70e0826bbb29bf4cf
Thanakorn Limpanawuthi <thnk2809@gmail.com>
2022-04-08 17:03:38 +0700

    Optimized Program

--- steganography.py
+++ steganography.py
@@ -2,6 +2,7 @@
 # 2. Wish      Suchalermkul 6288135 Sec.2
 
 from PIL import Image,ImageOps
+
 def changeNum(pixel):
     if pixel % 2 == 0:
         return pixel + 1
@@ -9,8 +10,7 @@ def changeNum(pixel):
         return pixel - 1
 
 # Convert message to ASCII
-def string_to_ascii():
-    text = input("Enter a message: ")
+def string_to_ascii(text):
     # Add stop string
     text += "!!!!####"
     ascii_values = []
@@ -53,25 +53,24 @@ def decode(decryptFile):
     px_decode = img_decode.load()
     row_decode,col_decode= img_decode.size
     decode_message = ""
-    #Loop through each pixel then each layers (R,G,B)
+    #Loop through each pixel then each layers (R,G,B) or (R,G,B,A)
+    data = ""
+    position = -1
     for i in range(row_decode):
         for j in range(col_decode):
             for k in px_decode[i,j]:
                 #Append the last significant bit
                 decode_message += (format(k,'08b'))[-1]
-    data = ""
-    for i in range (0,len(decode_message),8):
-        # If it found the stop string
-        # Then stop the loop
-        if data[-8:] == '!!!!####':
-            break
-        # Group the bit into group of 8 (1 Byte)
-        temp = decode_message[i:i+8]
-        # Then convert it to int (ASCII)
-        temp2 = int(temp, 2)
-        # Then convert it to character and append
-        data += (chr(temp2))            
-    return data[:-8]
+                position += 1
+                if (position % 8 == 0 and position / 8 != 0):
+                    temp = decode_message[position-8:position]
+                    temp2 = int(temp,2)
+                    data += (chr(temp2))
+                if data[-8:] == '!!!!####':
+                    return data[:-8]
+
+def calculateTotalbit():
+    return row*col*len(px[0,0])
 
 print("Please Select Mode:")
 print("Encode: 1")
@@ -83,10 +82,15 @@ if mode == 1:
     img = Image.open(inputFile, mode='r')
     row,col= img.size
     px = img.load()
-    encrpyt((string_to_ascii()))
-    outputFile = input("Enter the Filename for encrpyted file: ")
-    img = img.save(outputFile)
-    print("Done")
+    totalBit = calculateTotalbit()
+    text = input("Enter a message: ")
+    if (totalBit/8 >= len(text) + 8):
+        encrpyt((string_to_ascii(text)))
+        outputFile = input("Enter the Filename for encrpyted file: ")
+        img = img.save(outputFile)
+        print("Done")
+    else:
+        print("Too Long Message")
 elif mode == 2:
     decryptFile = input("Enter the Filename for decrypt: ")
     print("Decrypting file...")
